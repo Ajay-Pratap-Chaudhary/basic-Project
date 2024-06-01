479a2e9a18c0ac7fd29736caf45a7b8c3adfedeb
Ajay Pratap Chaudhary <ajay.pratap.chaudhary@outlook.com>
2024-06-01 10:07:57 +0530

    this is my small project to use animation css property via js

---
+++ index.js
@@ -0,0 +1,26 @@
+const img=document.getElementsByTagName("img");
+
+function ON()
+{
+   img[0].style.animationDuration="2s";
+}
+function OFF()
+{
+   img[0].style.animationDuration="0s";
+}
+function f1()
+{
+   img[0].style.animationDuration="1s";
+}
+function f2()
+{
+   img[0].style.animationDuration="0.5s";
+}
+function f3()
+{
+   img[0].style.animationDuration="0.3s";
+}
+function f4()
+{
+   img[0].style.animationDuration="0.1s";
+}
